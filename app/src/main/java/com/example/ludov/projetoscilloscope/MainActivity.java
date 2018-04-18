c1106d7814caefdb9ee430bae3d66b58e829b7c1
ludovic HO <ludovicho91@gmail.com>
2018-04-18 17:12:54 +0200

    inflation du menu

--- app/src/main/java/com/example/ludov/projetoscilloscope/MainActivity.java
+++ app/src/main/java/com/example/ludov/projetoscilloscope/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ludov.projetoscilloscope;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    
 }

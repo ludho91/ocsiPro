88fc024eb49d66a76302a1d86890cd3f46100a91
ludovic HO <ludovicho91@gmail.com>
2018-04-19 16:58:35 +0200

    vérification des autorisations bluetooth et localisation

--- app/src/main/java/com/example/ludov/projetoscilloscope/MainActivity.java
+++ app/src/main/java/com/example/ludov/projetoscilloscope/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ludov.projetoscilloscope;
 
+import android.Manifest;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +21,10 @@ import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
 public class MainActivity extends AppCompatActivity {
 
     //codes d'érreur
+    Intent intent;
     static private final int NO_BLUETOOTH = 100;
+    private int userResponse;
+    private String permissions[] = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN,Manifest.permission.ACCESS_FINE_LOCATION};
 
     private BluetoothAdapter mBluetoothAdapter;
     @Override
@@ -35,34 +42,63 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+
+        if(bluetoothRights() == NO_BLUETOOTH)
+            Toast.makeText(this, "pas De bluetooth", Toast.LENGTH_SHORT).show();
         if(bluetoothRights() == PERMISSION_GRANTED)
-            Toast.makeText(this, "bluetooth", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "bluetooth autorisé", Toast.LENGTH_SHORT).show();
         if(bluetoothRights() == PERMISSION_DENIED)
-            Toast.makeText(this, "pas blueetooth", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "blueetooth refusé", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
     private int bluetoothRights()
     {
 
+
         if (BluetoothAdapter.getDefaultAdapter() == null){
             return NO_BLUETOOTH;
         }
 
-        if(Build.VERSION.SDK_INT > 23)
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
         {
-            if(ContextCompat.checkSelfPermission(this,BLUETOOTH_SERVICE) == PERMISSION_GRANTED )
+            //demande d'autorisations si besoins
+            for (String permission:permissions)
             {
-                return PERMISSION_GRANTED;
+                if(checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED)
+                    requestPermissions(new String[]{permission}, userResponse);
+
             }
-            else
+            for (String permission:permissions)
             {
-                return PERMISSION_DENIED;
+                if(checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED)
+                    return PERMISSION_DENIED;
+
             }
 
+
         }
+        else
+            return PERMISSION_GRANTED;
+
+
         return 0;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
 }
+
 
 

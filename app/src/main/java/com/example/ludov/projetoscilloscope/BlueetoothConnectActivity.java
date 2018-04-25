960ec5102ed5310100c4bd7e58710c280db10180
ludovic HO <ludovicho91@gmail.com>
2018-04-25 14:55:13 +0200

    insertion d'une toolbar et bar de progression

--- app/src/main/java/com/example/ludov/projetoscilloscope/BlueetoothConnectActivity.java
+++ app/src/main/java/com/example/ludov/projetoscilloscope/BlueetoothConnectActivity.java
@@ -2,6 +2,8 @@ package com.example.ludov.projetoscilloscope;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class BlueetoothConnectActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class BlueetoothConnectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blueetooth_connect);
+        Toolbar mToolbar = findViewById(R.id.mToolbar);
+        setSupportActionBar(mToolbar);
     }
 }

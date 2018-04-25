5251df279bf2e95e029dba8b1189ad171f278833
ludovic HO <ludovicho91@gmail.com>
2018-04-25 12:21:48 +0200

    Création d'une activité pour la connexion blueetooth

---
+++ app/src/main/java/com/example/ludov/projetoscilloscope/BlueetoothConnectActivity.java
@@ -0,0 +1,13 @@
+package com.example.ludov.projetoscilloscope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlueetoothConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blueetooth_connect);
+    }
+}

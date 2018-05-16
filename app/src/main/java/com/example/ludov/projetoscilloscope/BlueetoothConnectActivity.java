7c1477ebceb2c7bacfcde2c1e089e321d4ade470
ludovic <ludovicho91@gmail.com>
2018-05-16 14:39:55 +0200

    Découverte et affichage des appareils bluetooth

--- app/src/main/java/com/example/ludov/projetoscilloscope/BlueetoothConnectActivity.java
+++ app/src/main/java/com/example/ludov/projetoscilloscope/BlueetoothConnectActivity.java
@@ -2,10 +2,15 @@ package com.example.ludov.projetoscilloscope;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,14 +22,42 @@ import java.util.Iterator;
 import java.util.Set;
 
 
-public class BlueetoothConnectActivity extends AppCompatActivity {
+public class BlueetoothConnectActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BluetoothAdapter mBluetoothAdapter;
     private ListView pairedDevicesListView, discoveredDevicesListView;
     private ProgressBar mProgressBar;
     private Button searching;
+    private IntentFilter filterDiscovered;
+    private IntentFilter filterEndDiscovering;
+    private ArrayAdapter<String> adapterDiscoveredDevice;
 
 
+    @Override
+    public void onClick(View view) {
+        this.mBluetoothAdapter.startDiscovery();
+
+
+        final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action))
+                {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    adapterDiscoveredDevice.add(device.getName());
+                }
+                if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+                {
+                    mBluetoothAdapter.cancelDiscovery();
+                    unregisterReceiver(this);
+                }
+            }
+        };
+
+        registerReceiver(mReceiver,filterDiscovered);
+        registerReceiver(mReceiver,filterEndDiscovering);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +67,45 @@ public class BlueetoothConnectActivity extends AppCompatActivity {
         Toolbar mToolbar = findViewById(R.id.mToolbar);
         setSupportActionBar(mToolbar);
 
-        //instanciation
+
+        //instanciations
+            //liste des objets appairés
         pairedDevicesListView = (ListView) findViewById(R.id.pairedDevicesView);
-        discoveredDevicesListView = (ListView) findViewById(R.id.listView2);
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        searching = (Button) findViewById(R.id.button);
-
-        //lancement du layout
-
-
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> setPairedDevices = mBluetoothAdapter.getBondedDevices();
         ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
         pairedDevicesListView.setAdapter(stringArrayAdapter);
 
+            //Pour la découverte d'appareils
+        discoveredDevicesListView = (ListView) findViewById(R.id.listView2);
+        filterDiscovered = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        filterEndDiscovering = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        adapterDiscoveredDevice = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        discoveredDevicesListView.setAdapter(adapterDiscoveredDevice);
+
+
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        searching = (Button) findViewById(R.id.button);
+
+
+
+        searching.setOnClickListener(this);
+
+
+
+
+
+
+        //affichage des appareils appairés
 
         for (BluetoothDevice device : setPairedDevices)
         {
             stringArrayAdapter.add(device.getName());
         }
+
+
+
 
 
     }
